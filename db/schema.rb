0c6cbcca34c45bcb84801020b90d394c3a9ee42d
Miko Taruc <miko.taruc@gmail.com>
2015-11-22 18:09:56 -0500

    Add list reference to item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122215503) do
+ActiveRecord::Schema.define(version: 20151122230858) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "list_id"
   end
+
+  add_index "items", ["list_id"], name: "index_items_on_list_id"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
